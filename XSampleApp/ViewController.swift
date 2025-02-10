b765625a7cbe23a999aae33cd4e314785deddead
Tomoko Tajima <karintococonut777@gmail.com>
2025-02-11 00:46:09 +0900

    Initial Commit

---
+++ XSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XSampleApp
+//
+//  Created by Tomoko T. Nakao on 2025/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XSampleAppTests/XSampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  XSampleAppTests.swift
+//  XSampleAppTests
+//
+//  Created by Tomoko T. Nakao on 2025/02/11.
+//
+
+import Testing
+@testable import XSampleApp
+
+struct XSampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
