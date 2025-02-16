760e21add07719c775b979c649f665c0f42b5084
Tomoko Tajima <karintococonut777@gmail.com>
2025-02-16 21:32:18 +0900

    HomeViewControllerにconfifureNavigationBar関数を設置

--- XSampleApp/HomeViewController.swift
+++ XSampleApp/HomeViewController.swift
@@ -7,12 +7,37 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+/// Home画面
+final class HomeViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - IBOutlets
+    
+    // MARK: - View Life-Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configureNavigationBar()
     }
-
+    
+    // MARK: - IBActions
+    
+    // MARK: - Other Methods
+    
+    private func configureNavigationBar() {
+        
+        // 画像ビューを作成
+        let imageView = UIImageView(image: UIImage(named: "ic_x_logo"))
+        imageView.contentMode = .scaleAspectFit
+        
+        // 画像のサイズを適切に設定
+        let titleView = UIView(frame: CGRect(x: 0, y: 0, width: 26, height: 24))
+        imageView.frame = titleView.bounds
+        titleView.addSubview(imageView)
+        
+        // ナビゲーションアイテムのタイトルビューに設定
+        self.navigationItem.titleView = titleView
+    }
 }
