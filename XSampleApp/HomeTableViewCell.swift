7cda9678ba3d93d4882b1cc1c3abe869d13e73ca
Tomoko Tajima <karintococonut777@gmail.com>
2025-02-20 23:22:34 +0900

    IBOutlet接続

--- XSampleApp/HomeTableViewCell.swift
+++ XSampleApp/HomeTableViewCell.swift
@@ -8,15 +8,22 @@
 import UIKit
 
 class HomeTableViewCell: UITableViewCell {
-
+    
+    ///ユーザーイメージビュー
+    @IBOutlet weak var userImageView: UIImageView!
+    ///ユーザー名ラベル
+    @IBOutlet weak var userNameLabel: UILabel!
+    ///本文ラベル
+    @IBOutlet weak var bodyLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
