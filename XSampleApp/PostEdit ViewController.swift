ee04a30a59c9226f599a294f473e8cf4510f068d
Tomoko Tajima <karintococonut777@gmail.com>
2025-02-24 01:54:56 +0900

    プロフイール画像、名前テキストフィールド、テキストビューをIBOutlet接続

--- XSampleApp/PostEdit ViewController.swift
+++ XSampleApp/PostEdit ViewController.swift
@@ -9,22 +9,29 @@ import UIKit
 
 ///ポスト編集画面
 class PostEdit_ViewController: UIViewController {
-
+    
+    ///自分のプロフイール画像
+    @IBOutlet private weak var userImageView: UIView!
+    ///名前テキストフィールド
+    @IBOutlet private weak var nameTextField: UITextField!
+    ///テキストビュー
+    @IBOutlet private weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
