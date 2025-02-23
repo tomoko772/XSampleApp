144034fe157e3dcddecde02e26ced39135e54b42
Tomoko Tajima <karintococonut777@gmail.com>
2025-02-24 01:28:12 +0900

    //  PostEdit ViewController.swift //  XSampleApp // //  Created by Tomoko T. Nakao on 2025/02/24. //
    import UIKit
    
    class PostEdit_ViewController: UIViewController {
    
        override func viewDidLoad() {
            super.viewDidLoad()
    
            // Do any additional setup after loading the view.
        }
    
        /*
        // MARK: - Navigation
    
        // In a storyboard-based application, you will often want to do a little preparation before navigation
        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
            // Get the new view controller using segue.destination.
            // Pass the selected object to the new view controller.
        }
        PostEditViewControllerを登録

---
+++ XSampleApp/PostEdit ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostEdit ViewController.swift
+//  XSampleApp
+//
+//  Created by Tomoko T. Nakao on 2025/02/24.
+//
+
+import UIKit
+
+///ポスト編集画面
+class PostEdit_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
