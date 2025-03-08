b29d2a8aa3e41633453b83b6553a76fe4b2b8732
Tomoko Tajima <karintococonut777@gmail.com>
2025-03-09 00:27:32 +0900

    Post.swiftを登録、RealmManagerに投稿を保存する処理を追加

---
+++ XSampleApp/Post.swift
@@ -0,0 +1,27 @@
+//
+//  Post.swift
+//  XSampleApp
+//
+//  Created by Tomoko T. Nakao on 2025/03/09.
+//
+
+import Foundation
+import RealmSwift
+
+/// ポストデータのモデル
+final class Post: Object {
+    /// データを一意に識別するための識別子
+    @Persisted var id: String = UUID().uuidString
+    /// 画像
+    @Persisted var imageString: String = ""
+    /// ポストした人の名前
+    @Persisted var name: String = ""
+    /// ポストの文章
+    @Persisted var body: String = ""
+    /// 日付
+    @Persisted var date = Date()
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
